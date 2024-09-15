56cfba0122b3deded3338beb2633b395d0772b94
GuillermoMorin <111553871+GuillermoMorin@users.noreply.github.com>
2024-09-15 17:02:22 -0600

    login

---
+++ login.js
@@ -0,0 +1,21 @@
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+
+    event.preventDefault();
+    
+    const usuario = document.getElementById("usuario").value;
+    const password = document.getElementById("password").value;
+
+    if (usuario === "1" && password === "1") {
+        window.location.href = "index.html";
+    } 
+    else if(usuario === "2" && password === "2"){
+        window.location.href = "instructor.html";
+    }
+    else if(usuario === "3" && password === "3"){
+        window.location.href = "admin.html";
+    }
+    else {
+        document.getElementById("errorMsg").textContent = "El usuario y/o la contraseña es incorrecta.";
+    }
+});
