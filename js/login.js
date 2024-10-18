2b5ec2675b42ba29c75e96490f37d3cdefddef8a
GuillermoMorin <111553871+GuillermoMorin@users.noreply.github.com>
2024-10-18 00:26:18 -0600

    Local Storage y Nuevo menú

--- js/login.js
+++ js/login.js
@@ -7,12 +7,21 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     const password = document.getElementById("password").value;
 
     if (usuario === "1" && password === "1") {
+        localStorage.setItem("ID_Usuario", 1);
+        localStorage.setItem("Nombre_Usuario", "Usuario1");
+        localStorage.setItem("Type_Usuario", 1);
         window.location.href = "index.html";
     } 
     else if(usuario === "2" && password === "2"){
+        localStorage.setItem("ID_Usuario", 2);
+        localStorage.setItem("Nombre_Usuario", "Usuario2");
+        localStorage.setItem("Type_Usuario", 2);
         window.location.href = "instructor.html";
     }
     else if(usuario === "3" && password === "3"){
+        localStorage.setItem("ID_Usuario", 3);
+        localStorage.setItem("Nombre_Usuario", "Usuario3");
+        localStorage.setItem("Type_Usuario", 3);
         window.location.href = "bloqueoDesbloqueo.html";
     }
     else {
