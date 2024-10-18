e4c9153d0abe3f7504d66fab55fbb100e30b5c87
GuillermoMorin <111553871+GuillermoMorin@users.noreply.github.com>
2024-10-18 00:31:37 -0600

    Js del nuevo navbar

---
+++ js/NavBarDefault.js
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded", function() {
+    const navbarContainer = document.createElement('div');
+    fetch('NavBarDefault.html')
+        .then(response => response.text())
+        .then(data => {
+            navbarContainer.innerHTML = data;
+            document.body.insertAdjacentElement('afterbegin', navbarContainer);
+        })
+        .catch(error => console.error('Error al cargar la barra de navegación:', error));
+});
